6d0d5a51c44c4828c786f40455c89b19c0ade115
brian <bybyby@gmail.com>
2018-08-22 01:21:11 -0700

    Completed.  Final 1. Added progress spinner. Check for no internet connection and TextView message.

--- app/src/main/java/com/example/brian/newsapp/MainActivity.java
+++ app/src/main/java/com/example/brian/newsapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.brian.newsapp;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -45,13 +48,28 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         articleListView.setAdapter(mAdapter);
 
 
-        // Get a reference to LoaderManger, in order to interact with loaders.
-        LoaderManager loaderManager = getLoaderManager();
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(ARTICLE_LOADER_ID, null, this);
 
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to LoaderManger, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(ARTICLE_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_spinner);
+            loadingIndicator.setVisibility(View.GONE);
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
 
 
 
@@ -84,6 +102,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<List<Article>> loader, List<Article> articles) {
+
+        // Set empty state text to display when no articles are found.
+        mEmptyStateTextView.setText(R.string.no_articles);
+
+        //Removes loading indicator now that onLoadFinished is called
+        findViewById(R.id.loading_spinner).setVisibility(View.GONE);
+
         // Clear the adapter of previous article data
         mAdapter.clear();
 
